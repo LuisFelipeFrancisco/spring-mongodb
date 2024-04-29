f220e84a6b2dba32deb2ed6be3588c8ef79ecf03
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-29 13:57:31 -0300

    feat: Posts comments

--- src/main/java/com/compass/springmongo/config/Instantiation.java
+++ src/main/java/com/compass/springmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.compass.springmongo.config;
 import com.compass.springmongo.domain.Post;
 import com.compass.springmongo.domain.User;
 import com.compass.springmongo.dot.AuthorDTO;
+import com.compass.springmongo.dot.CommentDTO;
 import com.compass.springmongo.repository.PostRepository;
 import com.compass.springmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,9 +41,17 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("31/01/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
+
         userRepository.save(maria);
 
     }
