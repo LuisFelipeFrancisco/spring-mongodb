9198bf40f61afb8af852edc0d6e29193b0de9108
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-29 11:53:12 -0300

    test: Instantiation config

---
+++ src/main/java/com/compass/springmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.compass.springmongo.config;
+
+import com.compass.springmongo.domain.User;
+import com.compass.springmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        repo.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repo.saveAll(Arrays.asList(maria,alex,bob));
+
+    }
+
+}
