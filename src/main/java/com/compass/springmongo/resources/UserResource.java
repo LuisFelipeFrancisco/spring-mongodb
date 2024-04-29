99f682c45a307893bf8371a779e9c09e19c385ae
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-29 12:33:43 -0300

    feat: User insert, delete and update

--- src/main/java/com/compass/springmongo/resources/UserResource.java
+++ src/main/java/com/compass/springmongo/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.compass.springmongo.dot.UserDTO;
 import com.compass.springmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +31,28 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok(new UserDTO(user));
+    }
+
+    @PostMapping(value = "/post")
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = service.fromDTO(userDTO);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDTO) {
+        User user = service.fromDTO(userDTO);
+        user.setId(id);
+        service.update(user);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/compass/springmongo/services/UserService.java
+++ src/main/java/com/compass/springmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.compass.springmongo.services;
 
 import com.compass.springmongo.domain.User;
+import com.compass.springmongo.dot.UserDTO;
 import com.compass.springmongo.repository.UserRepository;
 import com.compass.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,37 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
 
-    public User findById(String id){
+    public User findById(String id) {
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+    public User insert(User user) {
+        return repo.insert(user);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return repo.save(newUser);
+    }
+
+    private void updateData(User newUser, User user) {
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 
 }
